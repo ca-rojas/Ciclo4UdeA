3b062a4a73935cfbb13ea1879b5d0c7f1f2fd455
ca-rojas <sublime_0walnuts@icloud.com>
2022-10-07 21:39:19 -0500

    First model and DB connection

--- VetyShop/back/server.js
+++ VetyShop/back/server.js
@@ -1,9 +1,14 @@
 const app=require("./app");
+const connectDatabase = require("./config/database");
 
 //Set the configuration file
-const dotenv=require("dotenv")
+const dotenv=require("dotenv");
 dotenv.config({path:"back/config/config.env"})
 
+//DB configuration
+connectDatabase();
+
+//call the server
 const server=app.listen(process.env.PORT, ()=>{
-    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo ${process.env.NODE_ENV}`)
+    console.log(`Server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
 })
