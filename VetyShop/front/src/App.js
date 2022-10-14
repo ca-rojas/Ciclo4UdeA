191197c2656d2ac96d80b94a2f1a5d9de7634d01
Cesar Rojas <sublime_0walnuts@icloud.com>
2022-10-14 16:03:29 -0500

    Fixed footer and header paths

--- VetyShop/front/src/App.js
+++ VetyShop/front/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
-import Header from './components/layout/Header';
-import { Footer } from './components/layout/Footer';
+import Header from './components/layout/header';
+import { Footer } from './components/layout/footer';
 
 
 function App() {
