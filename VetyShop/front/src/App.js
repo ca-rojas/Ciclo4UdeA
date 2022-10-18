a3f5e86a40cd7b4b81dff577a953538652b0de7b
ca-rojas <sublime_0walnuts@icloud.com>
2022-10-18 07:56:04 -0500

    October 18 class

--- VetyShop/front/src/App.js
+++ VetyShop/front/src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 import React from 'react';
 import Header from './components/layout/header';
+import Home from './components/Home';
 import { Footer } from './components/layout/footer';
-
+// import router for the Front, this is different form the router in the back
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <center>Contenido aqui</center>
-        <Footer />
-    </div>
+    <Router>
+      <div className="App">
+          <Header />
+          <div  className='container container-fluid'>
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/Home" element={<Home />}/>
+            </Routes>
+          </div>
+          <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ VetyShop/front/src/components/Home.js
@@ -0,0 +1,83 @@
+import React, { Fragment } from 'react'
+
+export const Home = () => {
+  return (
+    <Fragment>
+        <h1 id="encabezado_productos">Ultimos Productos</h1>
+
+        <section id="productos" className='container mt-5'>
+            <div className='row'>
+                {/* producto 1  */}
+                <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                    <div className='card p-3 rounded'>
+                        <img className='card-img-top mx-auto' src='./images/nutra_gold.jpeg' alt="Nutra gold"></img>
+                        <div className='card-body d-flex flex-column'>
+                            <h5 id="titulo_producto"><a href='http://localhost:3000'>Nutra Gold Holistic Gatos</a></h5>
+                            <div className='rating mt-auto'>
+                                <div className='rating-outer'>
+                                    <div className='rating-inner'></div>
+                                </div>
+                                <span id="No_de_opiniones"> 5 reviews</span>
+                            </div>
+                            <p className='card-text'>$72.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>Ver detalle</a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* producto 2 */}
+                <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                    <div className='card p-3 rounded'>
+                        <img className='card-img-top mx-auto' src='./images/nutra-nuggets-mantenimiento.jpeg' alt="Nutra gold"></img>
+                        <div className='card-body d-flex flex-column'>
+                            <h5 id="titulo_producto"><a href='http://localhost:3000'>Nutra Gold Holistic Gatos</a></h5>
+                            <div className='rating mt-auto'>
+                                <div className='rating-outer'>
+                                    <div className='rating-inner'></div>
+                                </div>
+                                <span id="No_de_opiniones"> 2 reviews</span>
+                            </div>
+                            <p className='card-text'>$52.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>Ver detalle</a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* producto 3 */}
+                <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                    <div className='card p-3 rounded'>
+                        <img className='card-img-top mx-auto' src='./images/nutra-nuggets-performance.jpeg' alt="Nutra gold"></img>
+                        <div className='card-body d-flex flex-column'>
+                            <h5 id="titulo_producto"><a href='http://localhost:3000'>Nutra Nuggets Performance</a></h5>
+                            <div className='rating mt-auto'>
+                                <div className='rating-outer'>
+                                    <div className='rating-inner'></div>
+                                </div>
+                                <span id="No_de_opiniones"> 12 reviews</span>
+                            </div>
+                            <p className='card-text'>$80.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>Ver detalle</a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* producto 4 */}
+                <div className='col-sm-12 col-md-6 col-lg-3 my-3'>
+                    <div className='card p-3 rounded'>
+                        <img className='card-img-top mx-auto' src='./images/nutra-nuggets-profesional.webp' alt="Nutra gold"></img>
+                        <div className='card-body d-flex flex-column'>
+                            <h5 id="titulo_producto"><a href='http://localhost:3000'>Nutra Nuggets Professional</a></h5>
+                            <div className='rating mt-auto'>
+                                <div className='rating-outer'>
+                                    <div className='rating-inner'></div>
+                                </div>
+                                <span id="No_de_opiniones"> 7 reviews</span>
+                            </div>
+                            <p className='card-text'>$75.000</p><a href='http://localhost:3000' id="view_btn" className='btn btn-block'>Ver detalle</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </Fragment>
+  )
+}
+export default Home
